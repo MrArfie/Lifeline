090a785ba9872ab4da6b9a1ac6eaf455288e1b0b
MrArfie <adrianroquefernando@gmail.com>
2025-04-08 00:56:30 +0800

    Disable vpn

--- admin_auth.php
+++ admin_auth.php
@@ -1,18 +1,18 @@
 <?php
 session_start();
 
-function isValidAdminIP($ip, $allowedOctets) {
-    $firstOctet = explode('.', $ip)[0]; // Get the first octet of user's IP
-    return in_array($firstOctet, $allowedOctets);
-}
+// function isValidAdminIP($ip, $allowedOctets) {
+//     $firstOctet = explode('.', $ip)[0]; // Get the first octet of user's IP
+//     return in_array($firstOctet, $allowedOctets);
+// }
 
-$adminIP = $_SERVER['REMOTE_ADDR']; // Get the user's IP
-$allowedOctets = [51, 94];
+// $adminIP = $_SERVER['REMOTE_ADDR']; // Get the user's IP
+// $allowedOctets = [51, 94];
 
-if (isValidAdminIP($adminIP, $allowedOctets) == false) {
-    http_response_code(403);
-    die("Access denied: You must be connected to the VPN. Your IP: " . $adminIP);
-}
+// if (isValidAdminIP($adminIP, $allowedOctets) == false) {
+//     http_response_code(403);
+//     die("Access denied: You must be connected to the VPN. Your IP: " . $adminIP);
+// }
 
 
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
